5d8ad6b3112d06ba6269bdde2b91977235fbdb45
user <user@users-MacBook-Pro.local>
2013-04-24 22:55:01 -0700

    updated lint usage

--- jakefile.js
+++ jakefile.js
@@ -1,11 +1,17 @@
-task("default", function() {
-    console.log("default");
+task("default", ["lint"], function() {
+
 });
 
 task("lint", function() {
 
+  
+  var list = new jake.FileList();
+  list.include("**/*.js");
+  list.exclude("node_modules");
+  list.exclude("build");
+
   var runner = require("./build/lint/lint_runner.js");
-  runner.validateFile("./jakefile.js");
+  runner.validateFileList(list);
 
 });
 
