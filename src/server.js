694c160ab15a1d5aeb8139875038162567af0d72
user <user@users-MacBook-Pro.local>
2013-05-06 16:41:05 -0700

    cleanup logging

--- src/server.js
+++ src/server.js
@@ -11,14 +11,12 @@
         var index = 0;
 
         server.on("request", function(request, response) {
-            console.log("Received request -- " + (++index) + " -- " + request.url);
+            //console.log("Received request -- " + (++index) + " -- " + request.url);
 
             response.end("<html><head><title>This is a test</title></head><body>hello, world</body></html>");
         });
 
         server.listen(8080);
-
-        console.log("server running");
     };
 
     exports.stop = function(callback) {
