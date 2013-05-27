76fd3ce21a5c414a5f5c03a22d45358cdc3f54d1
fschwiet <fschwiet@gmail.com>
2013-05-27 15:42:33 -0700

    add run-times to the jake tracing

--- jakefile.js
+++ jakefile.js
@@ -9,13 +9,28 @@ var rimraf = require("rimraf");
 var util = require("util");
 var Q = require("q");
 
+var taskRuntimes = [];
+
 function tracedTask(name) {
   var result = task.apply(this, arguments);
+  var start;
 
   result.addListener('start', function() {
     console.log("\nExecuting " + name);
+    start = new Date().getTime();
+  });
+
+  result.addListener('complete', function() {
+    taskRuntimes.push({task:name, ms:new Date().getTime() - start});
   });
 }
+
+jake.addListener('complete', function() {
+  console.log("Execution time summary");
+  taskRuntimes.forEach(function(value) {
+    console.log("  " + value.task + " (" + value.ms + "ms)");
+  });
+});
 
 tracedTask("default", ["lint", "test"], function() {
 
