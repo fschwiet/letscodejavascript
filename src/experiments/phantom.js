3948bdc9bdf1259916dc9a9df988f1fae4c2a741
fschwiet <fschwiet@gmail.com>
2013-05-28 15:53:57 -0700

    trying to phantomjs synchronously

--- src/experiments/phantom.js
+++ src/experiments/phantom.js
@@ -1,8 +1,49 @@
+var Q = require("q");
 
 var phantom=require('node-phantom');
 
-phantom.create(function(err,ph) {
-  return ph.createPage(function(err,page) {
+function _createPageSync() {
+  var deferred = Q.defer();
+
+  this.createPage(function(err, page) {
+    if (err !== null){
+      deferred.reject(err);
+    } else {
+      deferred.resolve(page);
+    }
+  });
+
+  return deferred.promise;
+}
+
+phantom.createSync = function() {
+  var deferred = Q.defer();
+
+  this.create(function(err,ph) {
+    if (err !== null)
+      deferred.reject(err);  // should be new Error()?
+    else
+    {
+      ph.createPageSync = _createPageSync;
+
+      deferred.resolve(ph);
+    }
+  });
+
+  return deferred.promise;
+}
+
+var cleanup = [];
+cleanup.run = function() {
+  cleanup.forEach(function(value) { value(); });
+}
+
+phantom.createSync().then(function(ph) {
+  cleanup.unshift(function() { 
+    ph.exit();
+  });
+  return ph.createPageSync();
+}).then(function(page) {
     return page.open("http://google.com/", function(err,status) {
       console.log("opened site? ", status);
 //      page.includeJs('http://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js', function(err) {
@@ -13,10 +54,10 @@ phantom.create(function(err,ph) {
             return document.title;
           }, function(err,result) {
             console.log(result);
-            ph.exit();
+            cleanup.run();
           });
 //        }, 5000);
 //      });
-    });
   });
 });
+
