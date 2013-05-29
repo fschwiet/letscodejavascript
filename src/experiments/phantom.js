da7ddac3d2b9da5ec0545315206c581a77541dd5
fschwiet <fschwiet@gmail.com>
2013-05-29 12:40:19 -0700

    extract phantom shim

--- src/experiments/phantom.js
+++ src/experiments/phantom.js
@@ -1,40 +1,7 @@
+
 var Q = require("q");
 
-var phantom=require('node-phantom');
-
-function promisify(nodeAsyncFn, context, modifier) {
-  return function() {
-    var args = args = Array.prototype.slice.call(arguments);
-      var defer = Q.defer()
-
-      args.push(function(err, val) {
-        if (err !== null) {
-          return defer.reject(err);
-        }
-
-        if (modifier)
-          modifier(val);
-
-        return defer.resolve(val);
-      });
-
-      nodeAsyncFn.apply(context || {}, args);
-
-      return defer.promise;
-  };
-};
-
-phantom.promise = {
-  create : promisify(phantom.create, phantom, function(ph) {
-    ph.promise = {
-      createPage : promisify(ph.createPage, ph, function(page) {
-        page.promise = {
-          open : promisify(page.open, page),
-          evaluate : promisify(page.evaluate, page)
-        };
-      })
-    }
-})};
+var phantom=require('../test/node-phantom-shim');
 
 phantom.promise.create()
 .then(function(ph) {
