525030bffb217b04606879668618bda019d5cadd
fschwiet <fschwiet@gmail.com>
2013-05-31 11:35:46 -0700

    shim is working as expected now

--- src/test/node-phantom-shim.js
+++ src/test/node-phantom-shim.js
@@ -55,7 +55,63 @@ phantom.promise = {
           var deferred = Q.defer();
 
           page.evaluate(function(s) {
-            return document.querySelectorAll(s).length;
+            var matches = document.querySelectorAll(s);
+            var count = matches.length;
+
+            if (count == 1) {
+
+              // http://stackoverflow.com/questions/6157929/how-to-simulate-mouse-click-using-javascript
+              var simulatedClick = function(target, options) {
+
+                var event = target.ownerDocument.createEvent('MouseEvents');
+                options = options || {};
+
+                //Set your default options to the right of ||
+                var opts = {
+                  type: options.type                  || 'click',
+                  canBubble:options.canBubble             || true,
+                  cancelable:options.cancelable           || true,
+                  view:options.view                       || target.ownerDocument.defaultView, 
+                  detail:options.detail                   || 1,
+                    screenX:options.screenX                 || 0, //The coordinates within the entire page
+                    screenY:options.screenY                 || 0,
+                    clientX:options.clientX                 || 0, //The coordinates within the viewport
+                    clientY:options.clientY                 || 0,
+                    ctrlKey:options.ctrlKey                 || false,
+                    altKey:options.altKey                   || false,
+                    shiftKey:options.shiftKey               || false,
+                    metaKey:options.metaKey                 || false, //I *think* 'meta' is 'Cmd/Apple' on Mac, and 'Windows key' on Win. Not sure, though!
+                    button:options.button                   || 0, //0 = left, 1 = middle, 2 = right
+                    relatedTarget:options.relatedTarget     || null
+                  };
+
+                //Pass in the options
+                event.initMouseEvent(
+                  opts.type,
+                  opts.canBubble,
+                  opts.cancelable,
+                  opts.view, 
+                  opts.detail,
+                  opts.screenX,
+                  opts.screenY,
+                  opts.clientX,
+                  opts.clientY,
+                  opts.ctrlKey,
+                  opts.altKey,
+                  opts.shiftKey,
+                  opts.metaKey,
+                  opts.button,
+                  opts.relatedTarget
+                  );
+
+                //Fire the event
+                target.dispatchEvent(event);
+              };
+
+              simulatedClick(matches[0]);
+            }
+
+            return count;
           }, function(err,count) {
 
             console.log("evaluate finished with count " + count);
@@ -66,8 +122,7 @@ phantom.promise = {
             } else if (count < 1) {
               deferred.reject(new Error("An element matching '" + selector + "' not found"));
             } else {
-              deferred.reject(new Error("click not yet implemented"));
-              //deferred.resolve();
+              deferred.resolve();
             }
           }, selector);
 
@@ -76,5 +131,7 @@ phantom.promise = {
       })
     };
 })};
+
+
 
 module.exports = phantom;
