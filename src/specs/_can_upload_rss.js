6b58df3cdb96e7e8a17de17daf99fadafaeb3322
fschwiet <fschwiet@gmail.com>
2013-06-03 09:47:44 -0700

    moar rss upload test

--- src/specs/_can_upload_rss.js
+++ src/specs/_can_upload_rss.js
@@ -18,13 +18,18 @@
 
       assert.equal(status, "success");
 
-      return page.promise.uploadFile('input[name=image]', fileToUpload)
-      .then(function() {
-
-      });
+      return page.promise.uploadFile('form.uploadRss input[name=image]', fileToUpload);
     })
     .then(function() {
-
+      return page.promise.clickElement('form.uploadRss input[type=submit]');
+    })
+    .then(function() {
+      return page.promise.get("content");
+    })
+    .then(function(content) {
+      assert.notEqual(content.indexOf("TEDTalks (video)"), -1, "Expected to find subscription title");
+      assert.notEqual(content.indexOf("http://feeds.feedburner.com/tedtalks_video"), -1, "Expected to find xml url.");
+      assert.notEqual(content.indexOf("http://www.ted.com/talks/list"), -1, "Expected to find html url.");
     });
 
   }));
