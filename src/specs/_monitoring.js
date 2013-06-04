cbc7cee167ebf13baa0e18608011702b5fbc10ef
fschwiet <fschwiet@gmail.com>
2013-06-04 11:33:39 -0700

    add test that monitoring should report if upload directory is writeable

--- src/specs/_monitoring.js
+++ src/specs/_monitoring.js
@@ -9,19 +9,23 @@
 
     setup.whenRunningTheServer(exports);
 
-    exports.test_canReportDatabaseStatus = function(test) {
-
-        var expectedPattern = (/Database status:(.*\(.*\))/mi);
+    exports.test_shouldReportDatabaseStatus = function(test) {
 
         downloadFile("http://localhost:" + port + "/status", function(statusCode, responseBody) {
             
-            var match = expectedPattern.exec(responseBody);
+            function checkStatus(expectedPattern, expectedStatus){
+                var match = expectedPattern.exec(responseBody);
 
-            test.ok(match !== null, "Did not find connection status string.");
+                test.ok(match !== null, "Did not find " + expectedPattern.toString());
 
-            if (match !== null) {
-                test.equal(match[1].trim(), "connected (localhost)");
+                if (match !== null) {
+                    test.equal(match[1].trim(), expectedStatus);
+                }
             }
+
+            checkStatus((/Database status:(.*\(.*\))/mi), "connected (localhost)");
+            checkStatus((/Upload path status:(.*\(.*\))/mi), "writeable");
+
             test.done();
         });
     };
