b5ac2d53f0575cfa56d43ce0afbdfcd45d9a3fd5
fschwiet <fschwiet@gmail.com>
2013-06-04 09:41:23 -0700

    waitUntil can use promises

--- src/test/_waitUntil.js
+++ src/test/_waitUntil.js
@@ -2,6 +2,7 @@
   "use strict";
 
   var assert = require("assert");
+  var Q = require("q");
   var setup = require("./setup.js");
   var waitUntil = require("./waitUntil.js");
 
@@ -10,6 +11,21 @@
 
     return waitUntil(function() { 
       return ++waits >= 5;
+    },500,1).then(function() {
+      assert.equal(waits, 5);
+    });
+  });
+
+  setup.qtest(exports, "should wait for promise evaluation to be true", function() {
+    var waits = 0;
+
+    return waitUntil(function() { 
+
+      var defer = Q.defer();
+
+      defer.resolve(++waits >= 5);
+
+      return defer.promise;
     },500,1).then(function() {
       assert.equal(waits, 5);
     });
--- src/test/waitUntil.js
+++ src/test/waitUntil.js
@@ -16,23 +16,41 @@ module.exports = function(toBeEvaluated, msTimeout, msInterval) {
     var endTime = new Date();
     endTime.setMilliseconds(msTimeout + endTime.getMilliseconds());
 
+    var handleResult = null;
     var waitAndTry = null;
+
+    handleResult = function(result) {
+        if(!result) {
+
+            if (new Date() > endTime) {
+                defer.reject("timed out");
+            }
+            else 
+            {
+                return waitAndTry();
+            }
+        }
+        else
+        {
+            defer.resolve();
+        }
+    };
 
     waitAndTry = function() {
         setTimeout(function() {
-            if(!toBeEvaluated()) {
 
-                if (new Date() > endTime) {
-                    defer.reject("timed out");
-                }
-                else 
-                {
-                    return waitAndTry();
-                }
+            var evaluationResult = toBeEvaluated();
+
+            if (typeof evaluationResult !== 'undefined' && evaluationResult !== null && typeof evaluationResult.then == "function") {
+
+                evaluationResult.then(handleResult, 
+                    function() { 
+                        //  TODO
+                    });
             }
-            else
+            else 
             {
-                defer.resolve();
+                handleResult(evaluationResult);
             }
         }, msInterval);
     };
