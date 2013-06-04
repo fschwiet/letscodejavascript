e574c6eca90ba1cd1d21aa45748f8cdc1d695a19
fschwiet <fschwiet@gmail.com>
2013-06-04 11:20:57 -0700

    noodling on deployment

--- src/iis/install.ps1
+++ src/iis/install.ps1
@@ -1,6 +1,8 @@
+param([string]$installDir, [string]$uploadDir)
 
 $installDir = (get-item .).fullname
 $rootDir = (get-item .).parent.fullname
+$uploadsDir = $installDir.fullname + ".uploads"
 
 if (-not (get-module carbo[n])) {
     throw "Install carbon first"
@@ -8,6 +10,7 @@ if (-not (get-module carbo[n])) {
 
 import-module carbon
 
-Grant-Permission -Identity "IIS_IUSRS" -Permission Modify -Path $rootDir
+Grant-Permission -Identity "IIS_IUSRS" -Permission Modify -Path $installDir
+Grant-Permission -Identity "IIS_IUSRS" -Permission Modify -Path $uploadsDir
 
 Install-IisWebsite -Name letscodejavascript -p $installDir -bindings "http/*:80:"
