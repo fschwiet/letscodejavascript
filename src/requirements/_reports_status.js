4017e9e00b290b098ec16c5f57ef55a447f585e0
fschwiet <fschwiet@gmail.com>
2013-06-05 16:20:55 -0700

    replacing usages of downloadFile with request

--- src/requirements/_reports_status.js
+++ src/requirements/_reports_status.js
@@ -5,6 +5,7 @@
     var downloadFile = require("../test/download-file");
     var nconf = require("../server/config.js");
     var statusChecker = require("./statusChecker.js");
+    var request = require("request");
 
     var port = nconf.get("testServer_port");
 
@@ -12,10 +13,15 @@
 
     exports.test_shouldReportIfConfigurationIsWorking = function(test) {
 
-        downloadFile("http://localhost:" + port + "/status", function(statusCode, responseBody) {
-            
-            statusChecker.assertStatusIsGood(responseBody);
+        request("http://localhost:" + port + "/status", function(err, response, body){
 
+            if (err !== null) {
+                test.ok(false, err.toString());
+            } else if (response.statusCode != 200) {
+                test.ok(false, "Expected 200 status code, actually was " + response.statusCode);
+            } else {
+                statusChecker.assertStatusIsGood(body);
+            }
             test.done();
         });
     };
--- src/server/_server.js
+++ src/server/_server.js
@@ -4,12 +4,11 @@
     "use strict";
 
     var nconf = require('./config.js');
+    var server = require("./server.js");
+    var request = require("request");
 
     var port = nconf.get("testServer_port");
 
-    var server = require("./server.js");
-
-    var downloadFile = require("../test/download-file");
 
     exports.test_earlyStopCallsAreOk = function(test) {
         server.stop();
@@ -28,9 +27,11 @@
         server.start(port, function() {
             var url = "http://localhost:" + port + "/";
 
-            downloadFile(url, function(statusCode, body) {
+            request(url, function(err, response, body) {
 
-                test.equal(statusCode, 200, "Expected 200 response code for url " + url);
+                test.ok(err === null, "Error: " + err);
+
+                test.equal(response.statusCode, 200, "Expected 200 response code for url " + url);
                 test.notEqual(-1, body.indexOf("this is homepage.html"));
 
                 server.stop();
@@ -44,9 +45,11 @@
         server.start(port, function() {
             var url = "http://localhost:" + port + "/non-existing";
 
-            downloadFile(url, function(statusCode, body) {
+            request(url, function(err, response, body) {
 
-                test.equal(statusCode, 404, "Expected 404 response code for url " + url);
+                test.ok(err === null, "Error: " + err);
+
+                test.equal(response.statusCode, 404, "Expected 404 response code for url " + url);
                 server.stop();
                 test.done();
             });
