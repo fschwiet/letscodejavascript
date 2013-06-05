1bfae86bc05a6cde66341e6648797fadd69b59d9
fschwiet <fschwiet@gmail.com>
2013-06-05 12:59:28 -0700

    try to fix working dir when running within iis

--- src/iis/iis_server.js
+++ src/iis/iis_server.js
@@ -1,6 +1,10 @@
 
+var path = require("path");
 
 var server = require("../server/server.js");
+
+var workingDirectory = path.resolve(__dirname, "../..");
+process.chdir(workingDirectory)
 
 server.start(process.env.PORT, function() {
 
