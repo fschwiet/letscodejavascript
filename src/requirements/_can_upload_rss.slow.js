0633e9b14c6d393507bb4358e2a54c139898515e
fschwiet <fschwiet@gmail.com>
2013-06-07 17:16:09 -0700

    test cleanup

--- src/requirements/_can_upload_rss.slow.js
+++ src/requirements/_can_upload_rss.slow.js
@@ -1,48 +1,45 @@
-(function() {
-  "use strict";
 
-  var setup = require("../test/setup");
-  var assert = require("assert");
-  var nconf = require("../server/config.js");
-  var Q = require("q");
-  var waitUntil = require("../test/waitUntil");
-  var path = require("path");
+var setup = require("../test/setup");
+var assert = require("assert");
+var nconf = require("../server/config.js");
+var Q = require("q");
+var waitUntil = require("../test/waitUntil");
+var path = require("path");
 
-  var fileToUpload = path.resolve(__dirname, "subscriptions.xml");
-  var port = nconf.get("testServer_port");
+var fileToUpload = path.resolve(__dirname, "subscriptions.xml");
+var port = nconf.get("testServer_port");
 
-  setup.whenRunningTheServer(exports);
+setup.whenRunningTheServer(exports);
 
-  setup.qtest(exports, "can upload rss", setup.usingPhantom(function(page) {
+setup.qtest(exports, "can upload rss", setup.usingPhantom(function(page) {
 
-    return page.promise.open("http://localhost:" + port + "/upload/from/google")
-    .then(function(status) {
+  return page.promise.open("http://localhost:" + port + "/upload/from/google")
+  .then(function(status) {
 
-      assert.equal(status, "success");
+    assert.equal(status, "success");
 
-      return page.promise.uploadFile('form.uploadRss input[type=file]', fileToUpload);
-    })
-    .then(function() {
-      return page.promise.clickElement('form.uploadRss input[type=submit]');
-    })
-    .then(function() {
+    return page.promise.uploadFile('form.uploadRss input[type=file]', fileToUpload);
+  })
+  .then(function() {
+    return page.promise.clickElement('form.uploadRss input[type=submit]');
+  })
+  .then(function() {
 
-      return waitUntil(function() {
-        return page.promise.evaluate(function() { return document.title; })
-          .then(function(title) {
-            console.log("title", title);
-            return title.indexOf("Upload complete") > -1;
-          });
-      }, 2000);
-    })
-    .then(function() {
-      return page.promise.get("content");
-    })
-    .then(function(content) {
-      assert.notEqual(content.indexOf("TEDTalks (video)"), -1, "Expected to find subscription title");
-      assert.notEqual(content.indexOf("http://feeds.feedburner.com/tedtalks_video"), -1, "Expected to find xml url.");
-      assert.notEqual(content.indexOf("http://www.ted.com/talks/list"), -1, "Expected to find html url.");
-    });
-  }));
-})();
+    return waitUntil("upload is complete",function() {
+      return page.promise.evaluate(function() { return document.title; })
+        .then(function(title) {
+          console.log("title", title);
+          return title.indexOf("Upload complete") > -1;
+        });
+    }, 2000);
+  })
+  .then(function() {
+    return page.promise.get("content");
+  })
+  .then(function(content) {
+    assert.notEqual(content.indexOf("TEDTalks (video)"), -1, "Expected to find subscription title");
+    assert.notEqual(content.indexOf("http://feeds.feedburner.com/tedtalks_video"), -1, "Expected to find xml url.");
+    assert.notEqual(content.indexOf("http://www.ted.com/talks/list"), -1, "Expected to find html url.");
+  });
+}));
 
