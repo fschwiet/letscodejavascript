19c794b5c16ceadfaff302800d364c7a43aa7b48
fschwiet <fschwiet@gmail.com>
2013-06-09 13:01:16 -0700

    moving rss code to a separate file

---
+++ src/server/rss.js
@@ -0,0 +1,74 @@
+
+var fs = require("fs");
+var xml2js = require("xml2js");
+var Q = require("q");
+
+var modelFor = require("./modelFor");
+
+module.exports = function(app) {
+    app.get("/upload/from/google", handleUploadFromGoogleRequest);
+    app.post("/upload/from/google", handleUploadFromGooglePostRequest);  
+};
+
+function handleUploadFromGoogleRequest(request, response) {
+
+    response.render('uploadFromGoogle', modelFor("Upload your Google RSS reader subscriptions", request));
+}
+
+function handleUploadFromGooglePostRequest(request, response, next) {
+
+    var subscriptionsPath = request.files.subscriptionsXml.path;
+
+    loadSubscriptionsFromGoogleXml(subscriptionsPath)
+    .then(function(rows) {
+        var model = modelFor("Upload complete", request);
+        model.rows = rows;
+
+        response.render('uploadedFromGoogle', model);
+    });
+}
+
+function loadSubscriptionsFromGoogleXml(filepath) {
+
+    var deferred = Q.defer();
+    var parser = new xml2js.Parser();
+
+    fs.readFile(filepath, function(err, data) {
+
+        if (err) {
+            deferred.reject(err);
+        }
+        else
+        {
+            parser.parseString(data, function(err, result) {
+
+                if (err) {
+                    deferred.reject(err);
+                }
+                else {
+                    var rows = [];
+                    
+                    try
+                    {
+                        result.opml.body[0].outline.forEach(function(row) {
+                            rows.push({
+                                name : row.$.title,
+                                xmlUrl : row.$.xmlUrl,
+                                htmlUrl : row.$.htmlUrl
+                            });
+                        });
+                    }
+                    catch(ex)
+                    {
+                        deferred.reject(ex);
+                        return;
+                    }
+
+                    deferred.resolve(rows);
+                }
+            });
+        }
+    });
+
+    return deferred.promise;
+}
--- src/server/server.js
+++ src/server/server.js
@@ -9,7 +9,6 @@
     var path = require('path');
 
     var database = require("./database.js");
-    var xml2js = require("xml2js");
 
     var express = require('express');
     var modelFor = require("./modelFor");
@@ -34,9 +33,9 @@
 
         app.get("/", handleHomepageRequest);
         app.get("/status", handleStatusRequest);
-        app.get("/upload/from/google", handleUploadFromGoogleRequest);
-        app.post("/upload/from/google", handleUploadFromGooglePostRequest);
 
+        require("./rss")(app);
+        
         app.use(errorHandler);
 
         server = http.createServer(app);
@@ -83,52 +82,6 @@
                 response.end();
             });
         });        
-    }
-
-    function handleUploadFromGoogleRequest(request, response) {
-
-        response.render('uploadFromGoogle', modelFor("Upload your Google RSS reader subscriptions", request));
-    }
-
-    function handleUploadFromGooglePostRequest(request, response, next) {
-
-        var parser = new xml2js.Parser();
-
-        fs.readFile(request.files.subscriptionsXml.path, function(err, data) {
-
-            if (err) {
-                return next(err);
-            }
-
-            parser.parseString(data, function(err, result) {
-
-                if (err) {
-                    return next(err);
-                }
-
-                var rows = [];
-                
-                try
-                {
-                    result.opml.body[0].outline.forEach(function(row) {
-                        rows.push({
-                            name : row.$.title,
-                            xmlUrl : row.$.xmlUrl,
-                            htmlUrl : row.$.htmlUrl
-                        });
-                    });
-                }
-                catch(ex)
-                {
-                    return next(ex);
-                }
-
-                var model = modelFor("Upload complete", request);
-                model.rows = rows;
-
-                response.render('uploadedFromGoogle', model);
-            });
-        });
     }
 })();
 
