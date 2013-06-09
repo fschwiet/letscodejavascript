2f049ce8a14d00ee3c2c99967e8d3cc7dc36e317
fschwiet <fschwiet@gmail.com>
2013-06-09 12:01:01 -0700

    using server port config in more places

--- src/test/_runServer.js
+++ src/test/_runServer.js
@@ -2,13 +2,16 @@
     "use strict";
 
     var setup = require("./setup");
+    var config = require("../server/config");
     var request = require("request");
 
     setup.whenRunningTheServer(exports);
 
+    var url = config.urlFor("/");
+
     exports.test_canRunServer = function(test) {
 
-        request("http://localhost:8081", function(err, response, responseBody) {
+        request(url, function(err, response, responseBody) {
             test.ifError(err);
             test.equal(response.statusCode, 200, "Expected 200 response code");
             test.ok(responseBody.indexOf("this is homepage.html") !== -1, "Should have marker indicating homepage");
