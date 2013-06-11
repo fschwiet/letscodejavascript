a5c68989d8bde44410b9dd94ce852f6b2632cfeb
fschwiet <fschwiet@gmail.com>
2013-06-11 13:11:27 -0700

    fix test

--- src/server/_modelFor.js
+++ src/server/_modelFor.js
@@ -4,20 +4,25 @@
 
     var modelFor = require("./modelFor");
 
+    function fakeRequest() {
+        return {
+            flash : function() { return {}; }
+        };
+    }
+
     exports.test_standard_model_includes_page_title = function(test) {
         var expectedTitle = "abc ABC 123";
-        var result = modelFor(expectedTitle, {});
+        var result = modelFor(expectedTitle, fakeRequest());
         test.equal(result.title, expectedTitle);
         test.done();
     };
 
     exports.test_standard_model_includes_isAuthenticated = function(test) {
 
-        var authenticatedRequest = {
-            user: {}
-        };
+        var authenticatedRequest = fakeRequest();
+        authenticatedRequest.user = {};
 
-        var unathenticatedRequest = {};
+        var unathenticatedRequest = fakeRequest();
 
         var authenticated = modelFor("", authenticatedRequest);
         var unauthenticated = modelFor("", unathenticatedRequest);
