bdde224edac32751684db20fd43177dc9ba60112
fschwiet <fschwiet@gmail.com>
2013-06-18 23:29:09 -0700

    jade _layout shouldn't blow up if flash messages haven't been loaded

--- src/server/modelFor.test.js
+++ src/server/modelFor.test.js
@@ -4,33 +4,43 @@
 
     var modelFor = require("./modelFor");
 
-    function fakeRequest() {
-        return {
-            flash: function() {
-                return {};
-            }
-        };
-    }
-
-    exports.test_standard_model_includes_page_title = function(test) {
+    exports["standard model includes page title"] = function(test) {
         var expectedTitle = "abc ABC 123";
-        var result = modelFor(expectedTitle, fakeRequest());
+        var result = modelFor(expectedTitle, {});
         test.equal(result.title, expectedTitle);
         test.done();
     };
 
-    exports.test_standard_model_includes_isAuthenticated = function(test) {
+    exports["standard model includes isAuthenticated"] = function(test) {
 
-        var authenticatedRequest = fakeRequest();
+        var authenticatedRequest = {};
         authenticatedRequest.user = {};
 
-        var unathenticatedRequest = fakeRequest();
+        var unathenticatedRequest = {};
 
         var authenticated = modelFor("", authenticatedRequest);
         var unauthenticated = modelFor("", unathenticatedRequest);
 
         test.equal(authenticated.isAuthenticated, true);
         test.equal(unauthenticated.isAuthenticated, false);
+        test.done();
+    };
+
+    exports["standard model doesn't blow up if flash is unavailable"] = function(test) {
+
+        var expectedFlash = {
+            info: 'foo far baz'
+        };
+
+        var request = {
+            flash: function() {
+                return expectedFlash;
+            }
+        };
+
+        var model = modelFor("", request);
+
+        test.equal(JSON.stringify(model.flash), JSON.stringify(expectedFlash));
         test.done();
     };
 })();
