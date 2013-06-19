5df6800c794bcdd6f7021f7eaabbba16855f2c1e
fschwiet <fschwiet@gmail.com>
2013-06-19 09:48:29 -0700

    test tweaks

--- src/server/posts.test.js
+++ src/server/posts.test.js
@@ -50,14 +50,29 @@ setup.qtest(testBlock, "Should return empty result for invalid feeds", function(
         });
 });
 
-setup.qtest(testBlock, "Should give error if the http request fails", function() {
+setup.qtest(testBlock, "loadFeeds should be able to load RSS feeds", function() {
+
+    return posts.loadFeeds("http://127.0.0.76:8081/rss")
+    .then(function(results) {
+
+        assert.equal(JSON.stringify(results), JSON.stringify([{
+                        feedName: "FeedForAll Sample Feed",
+                        postName: "RSS Solutions for Restaurants",
+                        postUrl: "http://www.feedforall.com/restaurant.htm",
+                        postDate: new Date("June 1, 2013")
+                    }
+                ]));
+    });
+});
+
+setup.qtest(testBlock, "loadFeeds should give error if the http request fails", function() {
 
     return setup.shouldFail(function() {
         return posts.loadFeeds("http://nonexistsantserver.coommmm/rss");
     }, "getaddrinfo ENOTFOUND");
 });
 
-setup.qtest(testBlock, "Should give error if the http request fails #2", function() {
+setup.qtest(testBlock, "loadFeeds should give error if the http request fails #2", function() {
 
     return setup.shouldFail(function() {
         return posts.loadFeeds("http://127.0.0.76:8081/notexistingPath");
