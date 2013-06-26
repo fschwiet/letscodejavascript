170e0352029a1a748a19ea5d6c7369ed294d7dbc
fschwiet <fschwiet@gmail.com>
2013-06-26 15:14:37 -0700

    lint

--- src/client/main.js
+++ src/client/main.js
@@ -12,11 +12,19 @@ require(["feeds", "reader", "trimPostsMonitor", "jquery", "css!clientLib/bootstr
     }
 
     // google analytics code
-    (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-    (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-    m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-    })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+    (function (i, s, o, g, r, a, m) {
+        i.GoogleAnalyticsObject = r;
+        i[r] = i[r] || function () {
+            (i[r].q = i[r].q || []).push(arguments);
+        };
+        i[r].l = 1 * new Date();
+        a = s.createElement(o);
+        m = s.getElementsByTagName(o)[0];
+        a.async = 1;
+        a.src = g;
+        m.parentNode.insertBefore(a, m);
+    })(window, document, 'script', '//www.google-analytics.com/analytics.js', 'ga');
 
     ga('create', 'UA-42050413-1', 'asimplereader.com');
-    ga('send', 'pageview');    
+    ga('send', 'pageview');
 });
