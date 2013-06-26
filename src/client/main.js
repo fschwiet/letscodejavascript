8f878d65853b3da4e937ee5e80a3a1a836f38872
fschwiet <fschwiet@gmail.com>
2013-06-25 19:11:31 -0700

    trimPostsMonitor is kinda hooked up

--- src/client/main.js
+++ src/client/main.js
@@ -1,9 +1,12 @@
-require(["feeds", "reader", "jquery", "css!clientLib/bootstrap/css/bootstrap"], function(feeds, Reader, $) {
+require(["feeds", "reader", "trimPostsMonitor", "jquery", "css!clientLib/bootstrap/css/bootstrap"], function(feeds, Reader, trimPostsMonitor, $) {
     feeds.initialize($("body"));
 
     window.mainInitialized = true;
 
     if (typeof window.subscribedFeeds !== "undefined") {
+
+        trimPostsMonitor.start($(".js-topContainer"), $(".js-postsContainer"), 12);
+
         window.feedReader = new Reader();
         window.feedReader.startReader($(".js-postsContainer"), window.subscribedFeeds);
     }
