83a28563a24128af096a204fc8d593ea00792039
fschwiet <fschwiet@gmail.com>
2013-08-06 15:17:23 -0700

    moar subscription test

--- src/server/feeds.test.js
+++ src/server/feeds.test.js
@@ -1,4 +1,5 @@
 var Q = require("q");
+var expect = require("expect.js");
 
 var config = require("../server/config.js");
 var login = require("../test/login.js");
@@ -11,6 +12,8 @@ setup.qtest(context, "Should be able to subscribe to an RSS feed", function() {
     var that = this;
     var page = this.page;
 
+    var subscribedUrl = that.rssServer.urlFor('/rss/url');
+
     return page.promise.open(config.urlFor("/feeds"))
     .then(function() {
         return login.doLogin(page);
@@ -18,9 +21,26 @@ setup.qtest(context, "Should be able to subscribe to an RSS feed", function() {
     .then(function() {
         return page.promise.evaluate(function(rssUrl) {
             $("form.subscribeForm input[name=rssUrl]").val(rssUrl);
-        }, that.rssServer.urlFor('/rss/url'));
+        }, subscribedUrl);
     })
     .then(function() {
         return page.promise.clickElement("form.subscribeForm input[type=submit]");
+    })
+    .then(function() {
+        return page.promise.evaluate(function() {
+            var subscription = $("data[data-rssurl]");
+            return {
+                count: subscription.length,
+                rssUrl: subscription.data("rssurl"),
+                title: subscription.find("span.js-title").text()
+            };
+        });
+    })
+    .then(function(subscriptionInfo) {
+        expect(subscriptionInfo).to.eql({
+            count:1,
+            rssUrl:subscribedUrl,
+            title:'foo'
+        });
     });
 });
