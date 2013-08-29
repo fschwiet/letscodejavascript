49440153a9de7409753fa01b92211b9f2b841642
fschwiet <fschwiet@gmail.com>
2013-08-29 13:44:30 -0700

    putting the kudusync stuff back in

--- deploy.cmd
+++ deploy.cmd
@@ -82,6 +82,12 @@ goto :EOF
 :Deployment
 echo Handling node.js deployment.
 
+:: 1. KuduSync
+IF /I "%IN_PLACE_DEPLOYMENT%" NEQ "1" (
+  call %KUDU_SYNC_CMD% -v 50 -f "%DEPLOYMENT_SOURCE%" -t "%DEPLOYMENT_TARGET%" -n "%NEXT_MANIFEST_PATH%" -p "%PREVIOUS_MANIFEST_PATH%" -i ".git;.hg;.deployment;deploy.cmd"
+  IF !ERRORLEVEL! NEQ 0 goto error
+)
+
 :: 2. Select node version
 call :SelectNodeVersion
 
