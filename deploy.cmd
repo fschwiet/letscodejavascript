45b760c0c528619ba6b60ef7f8550dd1f35028fa
fschwiet <fschwiet@gmail.com>
2013-08-29 12:33:34 -0700

    underp

--- deploy.cmd
+++ deploy.cmd
@@ -102,7 +102,7 @@ IF EXIST "%DEPLOYMENT_TARGET%\package.json" (
 call !NODE_EXE! --version
 call !NODE_EXE! node_modules\jake\bin\cli.js prepareTempDirectory
 IF !ERRORLEVEL! NEQ 0 goto error
-call !NODE_EXE! node node_modules\jake\bin\cli.js buildClientBundle
+call !NODE_EXE! node_modules\jake\bin\cli.js buildClientBundle
 IF !ERRORLEVEL! NEQ 0 goto error
 
 
