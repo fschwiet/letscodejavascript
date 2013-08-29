4ae9d4d200525531f12488edff0600007880c8f9
fschwiet <fschwiet@gmail.com>
2013-08-29 11:35:22 -0700

    trying to get custom version working

--- server.js
+++ server.js
@@ -2,5 +2,5 @@ var http = require('http');
 var port = process.env.port || 1337;
 http.createServer(function(req, res){
     res.writeHead(200, {'Content-Type':'text/plain'});
-    res.end('Hello World\n');
+    res.end('Hello World\n' + process.version);
 }).listen(port);
