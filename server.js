df1f2682af7becc091e9a0e2a47541ef719a7c59
fschwiet <fschwiet@gmail.com>
2013-08-29 11:41:20 -0700

    try to start the right server

--- server.js
+++ server.js
@@ -1,6 +1,1 @@
-var http = require('http');
-var port = process.env.port || 1337;
-http.createServer(function(req, res){
-    res.writeHead(200, {'Content-Type':'text/plain'});
-    res.end('Hello World\n' + process.version);
-}).listen(port);
+require("./src/iis/iis_server.js");
