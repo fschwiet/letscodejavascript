ad16075f238e607dd1b60dc2bc08ae02c5159be2
fschwiet <fschwiet@gmail.com>
2013-08-29 13:18:25 -0700

    removing this step as its not working

--- deploy.cmd
+++ deploy.cmd
@@ -105,8 +105,6 @@ call !NODE_EXE! node_modules\jake\bin\cli.js prepareTempDirectory
 IF !ERRORLEVEL! NEQ 0 goto error
 call !NODE_EXE! node_modules\jake\bin\cli.js compileJadeViews
 IF !ERRORLEVEL! NEQ 0 goto error
-call !NODE_EXE! node_modules\requirejs\bin\r.js -o src\client\app-build-original
-IF !ERRORLEVEL! NEQ 0 goto error
 
 
 ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
