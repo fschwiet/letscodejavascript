149ab296bb55b55f1029d72b1c3473d740ef7ce6
fschwiet <fschwiet@gmail.com>
2013-09-03 16:55:13 -0700

    have a component for figuring out what to do with refer header

---
+++ src/server/auth.js
@@ -0,0 +1,25 @@
+
+var url = require('url');
+
+exports.withLoginPage = function(loginPath) {
+    return {
+        //
+        //  We ignore the referer header if its missing or matches our login page.
+        //
+        handleRefererUrl: function(req, res, next) {
+            var referer = req.header("referer");
+
+            if (typeof referer == 'string') {
+
+                var parsedReferer = url.parse(referer);
+
+                if (parsedReferer.pathname != loginPath) {
+
+                    req.session.referer = referer;
+                }
+            }
+
+            next();
+        }        
+    };
+};
---
+++ src/server/auth.test.js
@@ -0,0 +1,62 @@
+
+var expect = require("expect.js");
+
+var auth = require("./auth.js");
+var config = require("./config.js");
+
+function getRequestWithReferer(referer, existingSessionReferer) {
+    return { 
+        header : function(name) {
+            if (name == 'referer')
+                return referer;
+            else
+                return null;
+        },
+        session: {
+            referer: existingSessionReferer
+        }
+    };
+}
+
+exports["Referer url is typically used"] = function(test) {
+
+    var expectedReferer = "http://www.cnn.com/";
+
+    var req = getRequestWithReferer(expectedReferer);
+
+    auth.withLoginPage("/login").handleRefererUrl(req, null, function() {
+
+        expect(req.session.referer).to.be(expectedReferer);
+
+        test.done();
+    });
+};
+
+exports["Referer url is ignored when not set"] = function(test) {
+
+    var expectedReferer = "http://www.cnn.com/";
+
+    var req = getRequestWithReferer(null, expectedReferer);
+
+    auth.withLoginPage("/login").handleRefererUrl(req, null, function() {
+    
+        expect(req.session.referer).to.be(expectedReferer);
+
+        test.done();
+    });
+};
+
+
+exports["Referer url is ignored when it matches the login page"] = function(test) {
+
+    var expectedReferer = "http://www.cnn.com/";
+
+    var req = getRequestWithReferer(config.urlFor("/login", { someQuery : "value"}), null);
+
+    auth.withLoginPage("/login").handleRefererUrl(req, null, function() {
+
+        expect(req.session.referer).to.be(null);
+
+        test.done();
+    });
+};
