1e8106f14da89252ad6bcdcd18e7c824f82c2f11
fschwiet <fschwiet@gmail.com>
2013-09-18 10:34:25 -0700

    trying a different spawn-process implementation

--- src/test/spawn-process.js
+++ src/test/spawn-process.js
@@ -34,11 +34,14 @@ function spawnOpenProcess(name, program, args, options) {
 
 function spawn(name, program, args, options) {
 
+    options = JSON.parse(JSON.stringify(options || {}));
+    options.stdio = "inherit";
+
+    var process = childProcess.spawn(program, args, options);
+
     var deferred = Q.defer();
 
-    var result = spawnOpenProcess(name, program, args, options);
-
-    result.on('close', function(code) {
+    process.on('exit', function(code) {
         if (code !== 0) {
             deferred.reject(new Error(name + " had non-zero exit code " + code));
         } else {
