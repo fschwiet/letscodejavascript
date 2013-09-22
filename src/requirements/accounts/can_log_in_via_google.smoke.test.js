1bfd4efc5d7a5daf5844b01fe915c2a97fa9e4b7
fschwiet <fschwiet@gmail.com>
2013-09-22 09:41:36 -0700

    minor

--- src/requirements/accounts/can_log_in_via_google.smoke.test.js
+++ src/requirements/accounts/can_log_in_via_google.smoke.test.js
@@ -3,12 +3,14 @@ var setup = require("../../test/setup");
 var config = require("../../server/config");
 var login = require("../../test/login.js");
 
+var context = setup.usingPhantomPage(exports);
+
 check_login_from(config.urlFor("/feeds"));
 check_login_from(config.urlFor("/status"));
 
 function check_login_from(startPage) {
 
-    setup.qtest(setup.usingPhantomPage(exports), "can log in with google credentials from " + startPage, function() {
+    context.test("can log in with google credentials from " + startPage, function() {
 
         var page = this.page;
 
