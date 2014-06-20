8b76431971909d6dbd8a7f8cdc840f38e5272845
Frank <fschwiet@gmail.com>
2014-06-20 11:38:11 -0700

    add workaround for iis

--- src/iis/iis_server.js
+++ src/iis/iis_server.js
@@ -7,8 +7,14 @@ server.start(process.env.PORT, function() {
     console.log("Server started.");
 });
 
-process.stdin.on('data', function() {
-    server.stop(function() {
-        process.exit();
+try {
+    process.stdin.on('data', function() {
+        server.stop(function() {
+            process.exit();
+        });
     });
-});
+}
+catch(e) {
+    // iisnode doesn't support process.stdin, but in that case we don't want
+    // to watch stdin anyhow.   https://github.com/tjanczuk/iisnode/issues/337
+}
