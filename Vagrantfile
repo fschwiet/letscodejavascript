9178bf7d59f88b38a2308117235a0753ebb4201a
Frank <fschwiet@gmail.com>
2014-08-14 16:06:15 -0700

    porting over some config

--- Vagrantfile
+++ Vagrantfile
@@ -9,16 +9,30 @@ wwwuserPassword = ENV["wwwuserPassword"] || "password"
 
 require "./util.rb"
 
+
 Vagrant.configure("2") do |config|
 
 	config.vm.box = "opscode-ubuntu-14.04"
 	config.vm.box_url = "http://opscode-vm-bento.s3.amazonaws.com/vagrant/virtualbox/opscode_ubuntu-14.04_chef-provisionerless.box"
+	megabytesMemoryInstalled = 512
 
 	config.omnibus.chef_version = :latest
 
 	config.vm.network "private_network", ip: "192.168.33.100"
 	config.vm.network "forwarded_port", guest: 8080, host: 8080
 	config.vm.synced_folder syncedFolder, "/vagrant"
+
+	enableFirewall config.vm, [
+		"21/tcp",    #ftp, used by wget during some provisioning
+		"22/tcp",    #ssh
+		"80/tcp",    #www
+		"8081/tcp",  #www testing
+		"3306/tcp"  #mysql
+	]
+
+	protectSshFromLoginAttacks config.vm
+
+	createSwapFileIfMissing config.vm, 2*megabytesMemoryInstalled
 
 	aptgetUpdate config.vm
 	installGit config.vm
--- util.rb
+++ util.rb
@@ -25,3 +25,68 @@ def installNodejs(vm)
 		}
 	end
 end
+
+def protectSshFromLoginAttacks(vm)
+	vm.provision "shell", inline: <<-EOS
+		sudo apt-get install -y fail2ban; 
+		sudo cp /etc/fail2ban/jail.conf /etc/fail2ban/jail.local; 
+		sudo service fail2ban restart
+	EOS
+end
+
+
+def createSwapFileIfMissing(vm,sizeInMegabytes)
+
+	# final size of dd result is bs * count
+
+	createScript = <<-EOS 
+
+		if [[ $(sudo swapon -s) != */* ]]
+		then
+			echo "Creating swap drive of $size megabytes"
+
+			## Create the swapfile
+			sudo dd if=/dev/zero of=/swapfile bs=1024 count=$sizek
+			sudo chown root:root /swapfile 
+			sudo chmod 0600 /swapfile
+			sudo mkswap /swapfile
+
+			## Configue the system to use the swapfile
+
+			# swapon works until next reboot
+			sudo swapon /swapfile
+			# /etc/stab is used after reboot
+			echo "/swapfile       none    swap    sw      0       0" >> /etc/fstab
+
+		else
+			echo "Swap drive already detected - none created."
+		fi
+
+	EOS
+
+	createScript = createScript.gsub("$size", sizeInMegabytes.to_s)
+
+	vm.provision "shell", inline: createScript
+end
+
+
+def enableFirewall(vm, allowed)
+
+	firewallConfig = <<-EOS
+		sudo apt-get install -y ufw
+		sudo ufw default deny incoming
+		sudo ufw default allow outgoing
+	EOS
+
+	for term in allowed
+		firewallConfig += "sudo ufw allow " + term + "\n"
+	end
+
+	firewallConfig += <<-EOS
+		sudo echo yes | ufw enable
+		sudo ufw status verbose
+	EOS
+
+	vm.provision "shell", inline: firewallConfig
+end
+
