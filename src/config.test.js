c5a3d9785814e1112b33a4e9dff8d9b85244d184
Frank <fschwiet@gmail.com>
2014-08-15 15:22:39 -0700

    isolate config tests resilient from changes to config values

--- src/config.test.js
+++ src/config.test.js
@@ -1,9 +1,39 @@
+var assert = require("assert");
+var nconf = require("nconf");
 var config = require("./config.js");
 
-exports["can generate url, optionally with querystring"] = function(test) {
-    test.equal(config.urlFor("/path"), "http://" + config.get("server_hostname") + ":" + config.get("server_external_port") + "/path");
-    test.equal(config.urlFor("/path", {
-                foo: "bar"
-            }), "http://" + config.get("server_hostname") + ":" + config.get("server_external_port") + "/path?foo=bar");
-    test.done();
-};
+var NodeunitBuilder = require("cauldron").nodeunit;
+
+
+var originalPort;
+var originalHostname;
+
+var context = NodeunitBuilder.createTestScopeExtender("modifying config temporarily", 
+    function(done) {
+
+        originalPort = nconf.get("server_external_port");
+        originalHostname = nconf.get("server_hostname");
+        done();
+    }, 
+    function(done) {
+
+        nconf.set("server_external_port", originalPort);        
+        nconf.set("server_hostname", originalHostname);        
+        done();
+    })(exports);
+
+
+
+
+context.test("can generate url, optionally with querystring", function() {
+
+    nconf.set("server_hostname", "example.org");
+    nconf.set("server_external_port", 80);
+
+    assert.equal(config.urlFor("/path"), "http://example.org/path");
+
+    nconf.set("server_external_port", 8081);
+
+    assert.equal(config.urlFor("/path", { foo: "bar"}), 
+        "http://example.org:8081/path?foo=bar");
+});
